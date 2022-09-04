abc284fa886e4314ddad5238e3fea4fc126c0762
davidkim10 <davidykim10@gmail.com>
2022-09-04 19:06:38 -0400

    Changed greeting

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const EmptyState: React.FC = () => {
   return (
     <div className="state-empty forecast-card" style={{ padding: '20px' }}>
-      <h3>Welcome!</h3>
+      <h3>Aloha!</h3>
       <p style={{ lineHeight: 2, fontSize: '14px' }}>
         Search for your local weather by entering your ZIP Code. You can also find your
         local weather by entering your city and state <em>(e.g. New York, NY)</em>.
