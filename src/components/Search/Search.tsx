254600aa606e6c4b462d8ce268dd5f9154c5011a
davidkim10 <davidykim10@gmail.com>
2022-09-05 11:50:14 -0400

    fix safari mobile bug

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ReactComponent as Icon } from './search-icon.svg';
+// import { ReactComponent as Icon } from './search-icon.svg';
+import searchIcon from './search-icon.svg';
 import './Search.css';
 
 export interface ISearchProps {
@@ -30,7 +31,8 @@ export const Search: React.FC<ISearchProps> = ({
         value={value}
       />
       <button className="search-btn" onClick={onClick}>
-        <Icon height="14px" width="14px" fill="#fff" />
+        <img className="icon" src={searchIcon} alt="search" />
+        {/* <Icon height="14px" width="14px" fill="#fff" /> */}
       </button>
     </div>
   );
